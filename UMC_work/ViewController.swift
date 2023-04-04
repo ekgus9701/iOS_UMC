72dcc2096b7c8f2ef01714cb82a70c3da680f6f9
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2023-04-05 00:43:17 +0900

    Week 1,2

---
+++ UMC_work/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UMC_work
+//
+//  Created by 한다현 on 2023/03/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ UMC_workUITests/UMC_workUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UMC_workUITestsLaunchTests.swift
+//  UMC_workUITests
+//
+//  Created by 한다현 on 2023/03/31.
+//
+
+import XCTest
+
+final class UMC_workUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
