d14cc3e0efb6d39c0f456f51b90ec440099c723c
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2023-04-10 16:26:10 +0900

    0410

--- UMC_work/ViewController.swift
+++ UMC_work/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var uiTextField: UITextField!
+    
+    @IBAction func didTapAction(_ sender: Any) {
+        
+        let data:String=uiTextField.text!
+        
+        print(data)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad")
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("viewWillAppear")
+        
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        print("viewDidAppear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        print("viewDidDisappear")
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        print("viewWillDisappear")
+    }
+    
+    
 }
 
